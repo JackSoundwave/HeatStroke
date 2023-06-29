7a35436b3fb10f9ee716b5165708de28380fb495
JackS <pavenkalaichelvam2002@gmail.com>
2023-06-30 07:52:24 +0800

    small bugs that prevented the game from compiling

--- HeatStrokeAlpha0.01/Assets/Scripts/MouseController.cs
+++ HeatStrokeAlpha0.01/Assets/Scripts/MouseController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
