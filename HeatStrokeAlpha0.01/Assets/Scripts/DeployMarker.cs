e9d9a76263b95a500e971cd22681df1dac7f1385
JackS <pavenkalaichelvam2002@gmail.com>
2023-07-20 18:21:58 +0800

    Default Deploy Area
    Added functionality to mark a default deploy zone

--- HeatStrokeAlpha0.01/Assets/Scripts/DeployMarker.cs
+++ HeatStrokeAlpha0.01/Assets/Scripts/DeployMarker.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class DeployMarker : MonoBehaviour
 {
+    public bool markDefaultDeploy;
     public List<int> tileNumbersToMark; // Public list to hold tile numbers to mark as "Deploy Tile"
+    private List<int> defaultZone;
 
     private MapManager mapManager;
     private List<KeyValuePair<Vector2Int, HideAndShowScript>> overlayTiles;
@@ -19,8 +22,20 @@ public class DeployMarker : MonoBehaviour
 
         // Subscribe to the grid generated event
         GameEventSystem.current.onGridGenerated += MarkTilesAsDeployArea;
+        if(markDefaultDeploy == true)
+        {
+            GameEventSystem.current.onGridGenerated += MarkDefaultDeployZone;
+        }
     }
 
+    private void OnDestroy()
+    {
+        GameEventSystem.current.onGridGenerated -= MarkTilesAsDeployArea;
+        if(markDefaultDeploy == true)
+        {
+            GameEventSystem.current.onGridGenerated -= MarkDefaultDeployZone;
+        }
+    }
     private void MarkTilesAsDeployArea()
     {
         // Get the overlay tiles from the map manager
@@ -41,6 +56,29 @@ public class DeployMarker : MonoBehaviour
             else
             {
                 Debug.LogWarning("Invalid index to mark: " + indexToMark);
+            }
+        }
+    }
+
+    private void MarkDefaultDeployZone()
+    {
+        defaultZone = new List<int> { 46, 45, 44, 43, 42, 41, 38, 37, 36, 35, 34, 33};
+        overlayTiles = mapManager.GetOverLayTiles();
+
+        foreach (int indexToMark in defaultZone)
+        {
+            if (indexToMark >= 0 && indexToMark < overlayTiles.Count)
+            {
+                KeyValuePair<Vector2Int, HideAndShowScript> tileEntry = overlayTiles[indexToMark];
+                HideAndShowScript tile = tileEntry.Value;
+
+                tile.isDeployTile = true;
+                tile.ShowTile();
+                tile.DyeTileYellow();
+            }
+            else
+            {
+                Debug.LogWarning("Default Zone could not be resolved");
             }
         }
     }
